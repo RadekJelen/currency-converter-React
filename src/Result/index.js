5b17fe3cdd4974d77bd4cbe9a3759965b20b95a5
Radosław Jeleń <jelu140@gmail.com>
2022-08-11 15:08:38 +0200

    adding a Result component

---
+++ src/Result/index.js
@@ -0,0 +1,13 @@
+import "./style.css"
+
+const Result = () => (
+  <>
+    <p className="result"></p>
+    <p className="textInfo">
+      * - pole wymagane.<br />
+      Kursy pochodzą ze strony Narodowego Banku Polskiego (Tabela z dnia 2022-06-27).
+    </p>
+  </>
+);
+
+export default Result
