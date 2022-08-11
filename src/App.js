6fb847bec263d2763f16844b8d3d79b3ad8a1b40
Radosław Jeleń <jelu140@gmail.com>
2022-08-11 15:05:55 +0200

    refactoring converter component

--- src/App.js
+++ src/App.js
@@ -1,47 +1,17 @@
-import Button from "./Button";
+
 import Converter from "./Converter";
 
 function App() {
   return (
-    <div>
+    <>
       <header>
         <h1>Kalkulator walutowy</h1>
       </header>
-
-      <Converter>
-        <form>
-          <p>
-            <label>
-              <span className="converter__labelText">Mam*:</span>
-              <input className="converter__field" type="number" step="0.01" min="1"
-                placeholder="wpisz kwotę w PLN" required autoFocus />
-            </label>
-          </p>
-          <p>
-            <label>
-              <span className="converter__labelText">Waluty:</span>
-              <select className="converter__field">
-                <option value="EUR">Euro (EURO)</option>
-                <option value="USD">Dolar amerykński (USD)</option>
-                <option value="GBP">Funt (GBP)</option>
-              </select>
-            </label>
-          </p>
-
-          <Button />
-
-          <p className="converter__textResult"></p>
-          <p className="converter__textInfo">
-            * - pole wymagane.<br />
-            Kursy pochodzą ze strony Narodowego Banku Polskiego (Tabela z dnia 2022-06-27).
-          </p>
-        </form>
-      </Converter>
-      
+      <Converter />
       <footer>
         <p> To też sam zrobiłem, serio &#128516;</p>
       </footer>
-    </div>
+    </>
   );
 };
 
--- src/Converter/index.js
+++ src/Converter/index.js
@@ -1,8 +1,30 @@
 import "./style.css"
+import Button from "../Button";
+import Result from "../Result";
 
-const Converter = ({ children }) => (
+const Converter = () => (
   <main className="converter">
-    {children}
+    <form>
+      <p>
+        <label>
+          <span className="converter__labelText">Mam*:</span>
+          <input className="converter__field" type="number" step="0.01" min="1"
+            placeholder="wpisz kwotę w PLN" required autoFocus />
+        </label>
+      </p>
+      <p>
+        <label>
+          <span className="converter__labelText">Waluty:</span>
+          <select className="converter__field">
+            <option value="EUR">Euro (EURO)</option>
+            <option value="USD">Dolar amerykński (USD)</option>
+            <option value="GBP">Funt (GBP)</option>
+          </select>
+        </label>
+      </p>
+      <Button />
+      <Result />
+    </form>
   </main>
 );
 
