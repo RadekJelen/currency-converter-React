43932898155a64befdb38c1247fa03e9b55933c5
Radosław Jeleń <jelu140@gmail.com>
2022-08-11 13:51:30 +0200

    adding the HTML framework of the project

--- src/App.js
+++ src/App.js
@@ -2,7 +2,43 @@
 
 function App() {
   return (
-    <div></div>
+    <div>
+      <header>
+        <h1>Kalkulator walutowy</h1>
+      </header>
+      <main className="converter">
+        <form className="js-converter">
+          <p>
+            <label>
+              <span className="converter__labelText">Mam*:</span>
+              <input className="js-amount converter__field" type="number" step="0.01" min="1"
+                placeholder="wpisz kwotę w PLN" required autoFocus />
+            </label>
+          </p>
+          <p>
+            <label>
+              <span className="converter__labelText">Waluty:</span>
+              <select className="js-select converter__field">
+                <option value="EUR">Euro (EURO)</option>
+                <option value="USD">Dolar amerykński (USD)</option>
+                <option value="GBP">Funt (GBP)</option>
+              </select>
+            </label>
+          </p>
+          <p>
+            <button className="button">Przelicz!</button>
+          </p>
+          <p className="converter__textResult js-result"></p>
+          <p className="converter__textInfo">
+            * - pole wymagane.<br/>
+              Kursy pochodzą ze strony Narodowego Banku Polskiego (Tabela z dnia 2022-06-27).
+          </p>
+        </form>
+      </main>
+      <footer>
+        <p> To też sam zrobiłem, serio &#128516;</p>
+      </footer>
+    </div>
   );
 }
 
