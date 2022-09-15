8aaefeba242c19cff49bc59ebcdb2dae652aca3a
Radosław Jeleń <jelu140@gmail.com>
2022-09-15 17:16:38 +0200

    transfering of functionality to custom hook

--- src/App/Form/Clock/index.js
+++ src/App/Form/Clock/index.js
@@ -1,30 +1,21 @@
-import { useState, useEffect } from "react";
+import { useCurrentDate } from "./useCurrentDate";
 import { StyledClock } from "./styled"
 
 const Clock = () => {
-  const [currentDate, setCurrentDate] = useState(new Date());
-
-  useEffect(() => {
-    const intervalId = setInterval(() => {
-      setCurrentDate(new Date());
-    }, 1000);
-    return () => {
-      clearInterval(intervalId);
-    };
-  }, []);
+  const currentDate = useCurrentDate();
 
   return (
     <StyledClock>
       Dzisiaj jest
       {" "}
-      {currentDate.toLocaleString( undefined, {
-      weekday: "long", 
-      day: "numeric", 
-      month: "long", 
-      hour: "numeric", 
-      minute: "numeric", 
-      second: "numeric" 
-    })}
+      {currentDate.toLocaleString(undefined, {
+        weekday: "long",
+        day: "numeric",
+        month: "long",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric"
+      })}
     </StyledClock>
   );
 };
